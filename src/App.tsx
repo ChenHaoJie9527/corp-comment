cc93ada7e5a41e5667d09d179e75f71dfded2108
ChenHaoJie9527 <a18520145493@163.com>
2024-07-05 22:26:22 +0800

    样式表格式化

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import Container from "./layouts/Container";
 import Footer from "./layouts/Footer";
 import HashtagList from "./layouts/HashtagList";
+import "./App.css"
 
 function App() {
   return (
-    <div>
+    <div className="app">
       <Footer />
       <Container />
       <HashtagList />
--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
 
-type Props = {}
+type Props = {};
 
 export default function Footer({}: Props) {
   return (
-    <div>Footer</div>
-  )
+    <footer className="footer">
+      <small>
+        <p>
+          &copy; Copyright by{" "}
+          <a target="_blank" href="https://bytegrad.com">
+            ByteGrad.com
+          </a>{" "}
+          Intended for learning or your portfolio.
+        </p>
+        <p>
+          <span className="u-bold u-italic">Not allowed</span> to use as your
+          own teaching material.
+        </p>
+      </small>
+    </footer>
+  );
 }
